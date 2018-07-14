3ce2b29d76d02261ec6678b896e2d5bbc1984914
Nicole Li <li.nicole160@gmail.com>
2018-07-14 00:43:02 -0700

    we outta here

--- popup.js
+++ popup.js
@@ -6,11 +6,30 @@ document.addEventListener('DOMContentLoaded', function() {
 
  // Checking when making a new post
  parent.addEventListener('click', function(element) {
-   // console.log(element);
    if (element.target && element.target.innerText === 'Share') {
+     var text = document.querySelector('span[data-text="true"]').innerHTML;
+     console.log(element);
      // console.log('Button clicked');
+     var xhr = new XMLHttpRequest();
+     xhr.open('POST', 'https://api.aylien.com/api/v1/sentiment');
+     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+     xhr.setRequestHeader('X-AYLIEN-TextAPI-Application-Key', '5e43d2e7d1bcd986d3e6e0cdea9290a4');
+     xhr.setRequestHeader('X-AYLIEN-TextAPI-Application-ID', 'e3a4214c');
+     xhr.send(`text=${encodeURIComponent(text)}`);
+     xhr.onreadystatechange = function() {
+       var DONE = this.DONE || 4;
+       if (this.readyState === DONE){
+         var parsedResponse = JSON.parse(this.response);
+         alert('in our '+ parsedResponse.subjectivity +' opinion, \n we are '
+         + parsedResponse.polarity_confidence.toFixed(2)*100 +
+         '% confident that your comment, \n "' + parsedResponse.text + '" is '
+          + parsedResponse.polarity);
+       }
+     };
      var r = confirm('Make sure you are posting something wholesome!');
-     if (!r) element.stopPropagation();
+     if (!r) {
+       element.stopPropagation();
+     };
    }
  }, false);
 
