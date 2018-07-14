91c9556ad606f9d0cdbed2344884fabeaee77586
Nicole Li <li.nicole160@gmail.com>
2018-07-13 18:43:53 -0700

    increased functionality by a billion

--- popup.js
+++ popup.js
@@ -1,12 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
  // console.log('Listening');
- var parent = document.querySelector('._3u16');
+
+ // Tracking entire page
+ var parent = document.querySelector('#globalContainer');
+
+ // Checking when making a new post
  parent.addEventListener('click', function(element) {
-   // console.log(element)
-   if (element.target && element.target.nodeName === 'BUTTON') {
+   // console.log(element);
+   if (element.target && element.target.innerText === 'Share') {
      // console.log('Button clicked');
      var r = confirm('Make sure you are posting something wholesome!');
      if (!r) element.stopPropagation();
    }
  }, false);
+
+ // Checking when commenting on a post
+ parent.addEventListener('keydown', function(key) {
+   // console.log(key);
+   if (key.keyCode === 13) {
+     key.preventDefault();
+     // console.log('enter key');
+     var r = confirm('Make sure you are posting something wholesome!');
+     if (!r) key.stopPropagation();
+   }
+ }, false);
+
 }, false);
